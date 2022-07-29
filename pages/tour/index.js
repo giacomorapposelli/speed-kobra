bdfc04e7d2d56668d0c71f9cc3395867b90929ad
giacomo.rapposelli <giacomo.rapposelli@booming-games.com>
2022-07-29 16:33:38 +0200

    media queries fix

--- pages/tour/index.js
+++ pages/tour/index.js
@@ -5,18 +5,20 @@ function Tour() {
   return (
     <div className="section">
       <h1>UPCOMING TOURDATES</h1>
-      <div className={classes.showContainer}>
-        <h3 className={classes.columnTitle}>DATE</h3>
-        <h3 className={classes.columnTitle}>LOCATION</h3>
-        <h3 className={classes.columnTitle}>CITY</h3>
-      </div>
-      {tourDates.map((show, index) => (
-        <div className={classes.showContainer} key={index}>
-          <p className={classes.column}>{show.date}</p>
-          <p className={classes.column}>{show.location}</p>
-          <p className={classes.column}>{show.city}</p>
+      <div className={classes.tourContainer}>
+        <div className={classes.showContainer}>
+          <h3 className={classes.columnTitle}>DATE</h3>
+          <h3 className={classes.columnTitle}>LOCATION</h3>
+          <h3 className={classes.columnTitle}>CITY</h3>
         </div>
-      ))}
+        {tourDates.map((show, index) => (
+          <div className={classes.showContainer} key={index}>
+            <p className={classes.column}>{show.date}</p>
+            <p className={classes.column}>{show.location}</p>
+            <p className={classes.column}>{show.city}</p>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
